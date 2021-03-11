b72313d85d6395753bf0bd84506ce29e39881cc6
Tinashe Dzemwa <tinashedzemwa300@gmail.com>
2021-03-11 21:37:30 +0200

    demo 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Flattern - v4.0.1
-* Template URL: https://bootstrapmade.com/flattern-multipurpose-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
